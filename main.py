47dceacaf1eb214563caaf38ea06962b9ba6deb4
chebv <chebv@mail.ru>
2021-09-26 20:32:24 +0300

    First commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-print(
-    len(
-        set(
-            map(
-                int,
-                input().split()
-            )
-        )
-    )
-)
-print("Hello world!")
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+def func(x):
+    return  x**2
+
+X = np.arange(0, 60000, dtype = float)
+Y = func(X)
+
+fig = plt.figure(figsize=(8,4))
+plt.scatter(X, Y)
+plt.show()
+
+
+print("2222")
